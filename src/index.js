f8d9f55711c110fe05376d4965696b0ea29aa50b
logicfirst <slee@logicfirstllc.com>
2021-01-24 15:17:52 -0500

    Almost there but update not working

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 const URL = 'http://localhost:3000/dogs'
+// const dogForm = document.querySelector('#dog-form')
 
 document.addEventListener('DOMContentLoaded', () => {
 
     getDogs()
-    //createDogTable()
 
 })
 
@@ -11,15 +11,8 @@ function getDogs(){
     fetch(URL)
         .then(response => response.json())
         .then(dogsData => dogsData.forEach(dog => createDogTable(dog)))
-        
-
-
+    
 }
-
-// function renderDogs(dog){
-// // console.log(dog)
-
-// }
 
 function createDogTable(dog){
    let table = document.querySelector('#table-body')
@@ -38,8 +31,43 @@ function createDogTable(dog){
    
    let tableDataButton = document.createElement('button')
    tableDataButton.innerText = "Edit Dog"
+   tableDataButton.addEventListener('click', (event) => {
+       editDog(dog)
+   })
    
    tableRow.append(tableDataName, tableDataBreed, tableDataSex, tableDataButton)
 }
 
+function editDog(dog){
 
+    const dogForm = document.querySelector('#dog-form')
+    dogForm.name.value = dog.name
+    dogForm.breed.value =dog.breed
+    dogForm.sex.value =dog.sex
+
+    dogForm.addEventListener('submit', (event) => {
+        event.preventDefault()
+        dogPatch(dog, event)
+    })
+    
+}
+
+function dogPatch(dog, event){
+
+    let newUpdate = {
+        name: event.target.name.value, 
+        breed: event.target.breed.value, 
+        sex: event.target.sex.value 
+    }
+
+    let reqPack = {
+        header: {"Content-Type": "application/json", "Accept": "application/json"},
+        method: "PATCH",
+        body: JSON.stringify(newUpdate)
+    }
+
+    fetch(URL+`/${dog.id}`, reqPack)
+        .then(resp => resp.json())
+        .then(updatedDog => createDogTable(updatedDog))
+
+}
