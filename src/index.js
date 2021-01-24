a61213468bdbf422dbb2eb87ba4a3dadd2cae559
ManBytesDog <ThomasJ.Marstiller@yahoo.com>
2021-01-24 12:41:41 -0500

    working together

--- src/index.js
+++ src/index.js
@@ -1,3 +1,45 @@
+const URL = 'http://localhost:3000/dogs'
+
 document.addEventListener('DOMContentLoaded', () => {
 
+    getDogs()
+    //createDogTable()
+
 })
+
+function getDogs(){
+    fetch(URL)
+        .then(response => response.json())
+        .then(dogsData => dogsData.forEach(dog => createDogTable(dog)))
+        
+
+
+}
+
+// function renderDogs(dog){
+// // console.log(dog)
+
+// }
+
+function createDogTable(dog){
+   let table = document.querySelector('#table-body')
+
+   let tableRow = document.createElement('tr')
+   table.appendChild(tableRow)
+   
+   let tableDataName = document.createElement('td')
+   tableDataName.innerText = dog.name
+   
+   let tableDataBreed = document.createElement('td')
+   tableDataBreed.innerText = dog.breed
+
+   let tableDataSex = document.createElement('td')
+   tableDataSex.innerText = dog.sex
+   
+   let tableDataButton = document.createElement('button')
+   tableDataButton.innerText = "Edit Dog"
+   
+   tableRow.append(tableDataName, tableDataBreed, tableDataSex, tableDataButton)
+}
+
+
